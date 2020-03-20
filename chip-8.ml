e6145cab7068c1c07d9d02cbf6e53d24129a35a7
Yul3n <aghilas.boussaa@laposte.net>
2020-03-20 20:53:59 +0000

    Started working on the first functions

---
+++ chip-8.ml
@@ -0,0 +1,44 @@
+(* First we declare the memory as arrays *)
+(*   The RAM is a 4096 byte array (4K)   *)
+let ram = Array.make 4096 0
+(* There are 16 registers *)
+let regs = Array.make 16 0
+(* Plus some special registers *)
+(* The program counter *)
+let pc = ref 0
+(*  The stack pointer  *)
+let sp = ref 0
+(* Plus two timers which get decreased every at a rate of 60Hz,
+   only if they are non-zero. They are used for timers and sounds *)
+let sound_timer = ref 0
+let delay_timer = ref 0
+(* Then we declare the I register which is mainly used for 
+   addresses. *)
+let i = ref 0
+(* Finally we declare the stack as an array of 16 values *)
+let stack = Array.make 16 0
+
+(* The standard ocaml ** function has type float -> float -> float
+   which causes problems because we need for the binary to int
+   conversion, so we declare a new one with type int -> int 
+   -> int. *)
+
+let rec ( *** ) a b =
+  match b with
+    0 -> 1
+  | 1 -> a
+  | _ -> a * (a *** (b-1))
+
+(* Here we declare a function to get 4 bits of an integer which
+   coresponds to a hexadecimal digit *)
+
+let rec get_4bits ?rel_pos:(rel_pos=0) position bits = 
+  let bit = (bits asr (position + rel_pos)) land 1 in
+  match rel_pos with 
+   4 -> 0
+  |_ ->
+bit * (2 *** rel_pos) + get_4bits (position + 1) bits ~rel_pos:(rel_pos + 1)
+ 
+let () =
+  let test = get_4bits 4 ram.(0) in 
+  print_int test
